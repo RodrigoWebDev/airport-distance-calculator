13245d5fc06996fa852d1ee5bfc640b2d7870cc1
Rodrigo Queiroz <rodrigo.queiroz@dbccompany.com.br>
2022-08-18 15:14:33 -0300

    remove comment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import Home from "./pages/home";
 
-/* const app = document.getElementById("app");
-ReactDOM.render(<Home />, app); */
-
 const root = createRoot(document.getElementById("app"));
 root.render(<Home />);
